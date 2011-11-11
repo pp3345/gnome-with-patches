15eb6d494f244e4296bac9ffd088d8c0ed0e7584
Adam Jackson <ajax@redhat.com>
2011-11-11 17:50:15 -0500

    abi-rebuild script updates

--- driver-abi-rebuild.sh
+++ driver-abi-rebuild.sh
@@ -3,13 +3,14 @@
 # Trivial script to rebuild drivers for ABI changes in the server
 # Run me after a new xserver has hit the buildroot
 
-mkdir abi-rebuild
+mkdir -p abi-rebuild
 pushd abi-rebuild
 
 repoquery --qf="%{name}" --whatrequires xserver-abi\* | xargs -n 1 fedpkg co
 for i in */ ; do
+    [ -e $i/dead.package ] && continue
     pushd $i
-    rpmdev-bumpspec -c "- ABI rebuild"
+    rpmdev-bumpspec -c "- ABI rebuild" *.spec
     fedpkg commit -c -p && fedpkg build --nowait
     popd
 done
